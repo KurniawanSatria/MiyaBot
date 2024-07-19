c8eae45de4648c6740e6d199d99f1cfea963a613
Kurniawan Satria <satganzdevs@gmail.com>
2024-07-19 18:31:42 +0000

    NEW STYLE?

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+// tippy.js
+tippy("[data-tippy-content]");
+
+// aos.js
+AOS.init({
+  duration: 700,
+  once: true,
+});
+
+// navbar burger
+document.addEventListener("DOMContentLoaded", () => {
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0,
+  );
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+
+  var playBtn = document.getElementById("playBtn");
+  var btnIcon = document.getElementById("play-btn-icon");
+  var audio = new Audio("../assets/About You.mp3");
+
+  playBtn.addEventListener("click", async() => {
+    if (audio.paused) {
+     await audio.play();
+      btnIcon.classList.remove("fa-play");
+      btnIcon.classList.add("fa-pause");
+    } else {
+      audio.pause();
+      btnIcon.classList.remove("fa-pause");
+      btnIcon.classList.add("fa-play");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(".navbar-burger").click(function () {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
+
+// navbar on scroll
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 700) {
+      $("nav").addClass("nav-w");
+      $(".navbar-menu").addClass("nav-w");
+      $(".navbar-item").addClass("nav-dark");
+      $(".navbar-link").addClass("nav-dark");
+      $(".navbar-burger").removeClass("has-text-white");
+      $(".navbar-burger").addClass("has-text-dark");
+    } else {
+      $("nav").removeClass("nav-w");
+      $(".navbar-menu").removeClass("nav-w");
+      $(".navbar-item").removeClass("nav-dark");
+      $(".navbar-link").removeClass("nav-dark");
+      $(".navbar-burger").removeClass("has-text-dark");
+      $(".navbar-burger").addClass("has-text-white");
+    }
+  });
+});
+
+// back to top
+var btn = $("#backtotop");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 100) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
+
+// copyright year
+document.getElementById("cp-year").innerHTML = new Date().getFullYear();
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
